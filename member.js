f948c6fca6da2d50756772d8c1ad316da9d0266f
Emil Møller <139823248+molleremil@users.noreply.github.com>
2023-07-24 21:01:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    console.log(member.skills);
+    console.log(member.skills[1]);
+    console.log(member.skills[2]);
+    console.log(member.skills[0]);
+}
